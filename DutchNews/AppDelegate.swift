66d6ca6646317fbe70fa9cc54e800d44d9c93797
Farshad Mousalou <farshadm90@gmail.com>
2020-09-13 01:45:03 +0430

    - restructured files and resources.

--- DutchNews/AppDelegate.swift
+++ DutchNews/AppDelegate.swift
@@ -16,10 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow()
-        window?.backgroundColor = UIColor.lightGray
-        window?.rootViewController = UIViewController()
-        window?.makeKeyAndVisible()
+        
 
         return true
     }
