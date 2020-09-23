9dbf349df87e094142ed5efab50e30a3c083b5a7
Farshad Mousalou <farshadm90@gmail.com>
2020-09-23 17:01:14 +0330

    - Added ArticleDetailViewController and ArticlePageViewController. - Added UI Objects relate to article details.

---
+++ DutchNews/Classes/ViewControllers/ArticleDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ArticleDetailViewController.swift
+//  DutchNews
+//
+//  Created by Farshad Mousalou on 9/23/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+
+class ArticleDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+}
---
+++ DutchNews/Classes/ViewControllers/ArticlesPageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ArticlesPageViewController.swift
+//  DutchNews
+//
+//  Created by Farshad Mousalou on 9/23/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+import Pageboy
+
+class ArticlePageViewController: PageboyViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
