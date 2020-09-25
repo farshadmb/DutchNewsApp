b9f00361a51bbefc7080cfb977ef1464aeb04913
Farshad Mousalou <farshadm90@gmail.com>
2020-09-25 20:33:02 +0330

    - Fixed DataSource for reloading and inserting a new items.

--- DutchNews/Classes/Utilites/RxHeadlinesDataSource.swift
+++ DutchNews/Classes/Utilites/RxHeadlinesDataSource.swift
@@ -14,14 +14,27 @@ import RxCocoa
 /// Helper Class for fixing the issue with `MagazineLayout`
 class RxHeadlinesDataSource<T>: RxCollectionViewSectionedReloadDataSource<T> where T: SectionModelType {
     
-//    override func collectionView(_ collectionView: UICollectionView, observedEvent: Event<Element>) {
-//        Binder(self) { dataSource, element in
-//        
-//            dataSource.setSections(element)
-//            collectionView.reloadData()
-//            collectionView.collectionViewLayout.invalidateLayout()
-//            
-//        }.on(observedEvent)
-//    }
+    override func collectionView(_ collectionView: UICollectionView, observedEvent: Event<Element>) {
+        Binder(self) { dataSource, element in
+            
+            let indices = (0..<element.count).compactMap({ $0 })
+            
+            collectionView.performBatchUpdates({
+                if dataSource.sectionModels.count == 0 {
+                    collectionView.insertSections(indices, animationStyle: .automatic)
+                }else if dataSource.sectionModels.count == element.count {
+                    collectionView.reloadSections(indices, animationStyle: .fade)
+                }
+                
+                dataSource.setSections(element)
+            }) { (finish) in
+                guard finish else {
+                    return
+                }
+                collectionView.collectionViewLayout.invalidateLayout()
+            }
+            
+        }.on(observedEvent)
+    }
     
 }
